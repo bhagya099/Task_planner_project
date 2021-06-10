a28cb4b0c6e4ce516bad3973a8b33fb1e2faa7b9
Bhagyashree <bhagya099@gmail.com>
2021-06-10 17:06:23 +1000

    added a newtask class and console.log it

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
-const task1 = new TaskManager();
-gi
+const NewTask = new TaskManager();
+
 // Validate Task Name, Description, AssignedTo, DueDate, Status. Values need to be more than 5 characters.
 //Declare Variables
 const nameInput = document.querySelector("#name");
@@ -8,42 +8,69 @@ const dateInput = document.querySelector("#date");
 const statusInput = document.querySelector("#status");
 const description = document.querySelector("#description");
 const submitButton = document.querySelector("#btnSub");
+const displayTask = document.querySelector("#displayTask");
 
 //create errMessageFunction
 showError = () => {
-  let errMessageName = document.querySelector("#errMsgName");
-  let errMessageAssign = document.querySelector("#errMsgAssign");
-  let errMessageDate = document.querySelector("#errMsgDate");
-  let errMessageDes = document.querySelector("#errMsgDes");
-  // input valid name
-  if (nameInput.value.length <= 5) {
-    errMessageName.innerHTML =
-      "Please enter a valid name more than 5 characters";
-  } else {
-    errMessageName.innerHTML = "";
-  }
-  // input valid assign
-  if (assignedInput.value.length <= 5) {
-    errMessageAssign.innerHTML =
-      "Please enter a valid name more than 5 characters";
-  } else {
-    errMessageAssign.innerHTML = "";
-  }
-  // input valid date
-  if (dateInput.value) {
-    errMessageDate.innerHTML = "";
-  } else {
-    errMessageDate.innerHTML = "Please Select the date";
-  }
-  // input valid description
-  if (description.value.length <= 5) {
-    errMessageDes.innerHTML =
-      "Please enter a valid name more than 5 characters";
-  } else {
-    errMessageDes.innerHTML = "";
-  }
+    let errMessageName = document.querySelector("#errMsgName");
+    let errMessageAssign = document.querySelector("#errMsgAssign");
+    let errMessageDate = document.querySelector("#errMsgDate");
+    let errMessageDes = document.querySelector("#errMsgDes");
+    // input valid name
+    if (nameInput.value.length <= 5) {
+        errMessageName.innerHTML =
+            "Please enter a valid name more than 5 characters";
+    } else {
+        errMessageName.innerHTML = "";
+    }
+    // input valid assign
+    if (assignedInput.value.length <= 5) {
+        errMessageAssign.innerHTML =
+            "Please enter a valid name more than 5 characters";
+    } else {
+        errMessageAssign.innerHTML = "";
+    }
+    // input valid date
+    if (dateInput.value) {
+        errMessageDate.innerHTML = "";
+    } else {
+        errMessageDate.innerHTML = "Please Select the date";
+    }
+    // input valid description
+    if (description.value.length <= 5) {
+        errMessageDes.innerHTML =
+            "Please enter a valid name more than 5 characters";
+    } else {
+        errMessageDes.innerHTML = "";
+    }
+
+
+
+    // use classes
+    NewTask.addTask(nameInput.value, description.value, assignedInput.value, dateInput.value, statusInput.value);
+    console.log(NewTask);
+    // displayTask.innerHTML = `<ul class="list-group">
+    //                           <li class="card mb-2" style="width: 100%">
+    //                            <div class="card-body">
+    //                           <h5 class="card-title">${nameInput.value}</h5>
+    // </div>
+    // <ul class="list-group list-group-flush">
+    //     <li class="list-group-item">${assignedInput.value}</li>
+    //     <li class="list-group-item">${dateInput.value}</li>
+    //     <li class="list-group-item">${statusInput.value}</li>
+    // </ul>
+    // <div class="card-body">
+    //     <button type="button " class="btn $btn-border-width:0 btn-success btn-sm">
+    //      Done
+    //      </button>
+    //     <button type="button " class="btn btn-danger btn-sm">
+    //      Delete
+    //    </button>
+    // </div>
+    //  </li>`
+
 };
-submitButton.addEventListener("click", showError);
+submitButton.addEventListener('click', showError);
 
 // Check if the Task Name input value is more than 5 characters.
 
