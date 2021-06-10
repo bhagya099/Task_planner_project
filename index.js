52ebef0d087ef6e9e1d3ee3703a74c691848a003
HowLargeTheTeeth <graham.emily15@gmail.com>
2021-06-10 10:38:39 +1000

    javascript added

---
+++ index.js
@@ -0,0 +1,16 @@
+// Validate Task Name, Description, AssignedTo, DueDate, Status. Values need to be more than 5 characters. 
+//Declare Variables
+const nameInput = document.querySelector('#name');
+const assignedInput = document.querySelector('#assigned');
+let dateInput = document.querySelector('#date');
+let statusInput = 
+
+
+// Check if the Task Name input value is more than 5 characters.
+
+// Check if the Task Description input value is more than 5 characters.
+// Check if the Assigned To value is more than 5 characters.
+// Check if the Task Due Date input value is not empty.
+// Check if the Task Status input value is not empty.
+// Step 3: Showing errors to users
+
